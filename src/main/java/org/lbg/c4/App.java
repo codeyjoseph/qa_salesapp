6a940882713e0ba6ac1f18b000e65c2658578ae5
and <and@and.com>
2024-10-24 16:02:08 +0100

    first commit

---
+++ src/main/java/org/lbg/c4/App.java
@@ -0,0 +1,67 @@
+package org.lbg.c4;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
+import java.math.*;
+import java.text.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    public static double calculatePrice(double item, double vat){
+        return item + (item*vat);
+    }
+
+
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String item;
+        boolean asked = false;
+        String answer;
+        double itemPrice = 0, itemPriceTotal = 0, itemVAT;
+
+        System.out.println("Hello, welcome to my sales applicatiion.");
+
+        while(true) {
+            try {
+                    if(!asked) {
+                        System.out.println("Type in the price of your item: ");
+
+                        item = input.nextLine();
+                        itemPrice += Double.parseDouble(item);
+
+                        System.out.println("Please type the VAT type of your item: ");
+                        item = input.nextLine();
+                        itemVAT = Double.parseDouble(item);
+
+                        itemPriceTotal += calculatePrice(itemPrice, itemVAT);
+
+                        asked = true;
+                    }
+                    System.out.println("Would you like to continue? if no type 'QUIT' if yes" +
+                            " press the ENTER key.");
+
+                    answer = input.nextLine();
+                    System.out.println(answer);
+
+                    if(answer.contains("Q")) {
+                        System.out.println("The total of your items is: " + df.format(itemPriceTotal));
+                        break;
+                    }
+
+                    if (answer.contains("B")){
+                            asked = false;
+
+                    }
+            } catch (Exception e) {
+                System.out.println("Incorrect Value entered, please try again.");
+            }
+        }
+    }
+}
